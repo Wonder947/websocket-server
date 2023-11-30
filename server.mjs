e0d736a75451038b16cceefdc3271edd6e6630c7
Wonder947 <123397090+Wonder947@users.noreply.github.com>
2023-11-30 18:31:26 -0500

    modify CORS, try deploy

--- server.mjs
+++ server.mjs
@@ -1,9 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config()
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+console.log(process.env.CORS_ALLOWED_ORIGIN)
+const io = new Server(server, {
+  cors: {
+    origin: [process.env.CORS_ALLOWED_ORIGIN, 'http://localhost:3000']
+  }
+});
 import url from 'url';
 import path from 'path';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -18,5 +25,12 @@ server.listen(PORT, ()=>{
 io.on('connection', (socket)=>{
     console.log("a client has connected:", socket.id)
 
+  socket.on('greeting', (data)=>{console.log(data)})
+
+  socket.on('test2', ()=>socket.emit('test2'))
+
+    setInterval(() => {
+      socket.emit('test')
+    }, 2000);
 })
 
